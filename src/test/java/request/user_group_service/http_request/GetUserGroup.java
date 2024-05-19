0f2824662f1577dd1096adff96f6e8c7d305aea9
Mohammed <Msqx71@gmail.com>
2024-05-19 17:20:59 +0300

    user group service

---
+++ src/test/java/request/user_group_service/http_request/GetUserGroup.java
@@ -0,0 +1,73 @@
+package request.user_group_service.http_request;
+
+import base_urls.QuasparepartsBaseUrl;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import request.user_group_service.pojo.UserGroupPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+import static request.user_group_service.http_request.CreateUserGroup.orgId;
+import static request.user_group_service.http_request.CreateUserGroup.userGroupId;
+import static utilities.ObjectMapperUtilities.convertJsonToJava;
+
+public class GetUserGroup extends QuasparepartsBaseUrl {
+
+    /*
+   Given
+       url: "https://qa-gm3.quaspareparts.com/a3m/auth/api:id
+
+   When
+       user send GET request
+   Then
+       verify status code is 200
+   And
+       response is like :
+       {
+    "id": 2202,
+    "name": "SDA_Group1",
+    "short_name": "SDAG1",
+    "description": "Testing POST request for creating new user group",
+    "group_type_id": 1,
+    "group_type": {
+        "id": 1,
+        "name": "Department",
+        "description": "List All Department, and not authorized to access any the application"
+    },
+    "organization_id": 1716027192963795
+    }
+
+    */
+    @Test(dependsOnMethods = {"request.user_group_service.http_request.CreateUserGroup.createUserGroupTest"})
+    public void getUserGroup() {
+
+        //Set the url
+        //spec.pathParams("first", "user-group", "second", userGroupId);
+
+        //Set Expected Data
+
+        UserGroupPojo expectedData = CreateUserGroup.payload;
+        expectedData.setId(userGroupId);
+        expectedData.setOrganization_id(orgId);
+
+
+        //Send the request and get the response
+        Response response = given(spec).when().get("user-group/"+userGroupId);
+        response.prettyPrint();
+
+
+        UserGroupPojo actualData = convertJsonToJava(response.asString(), UserGroupPojo.class);
+
+        System.out.println("actualData.getOrganization_id() = " + actualData.getOrganization_id());
+        //Assertions
+        assertEquals(200, response.statusCode());
+        assertEquals(CreateUserGroup.userGroupId, actualData.getId());
+        assertEquals(expectedData.getName(), actualData.getName());
+        assertEquals(expectedData.getShort_name(), actualData.getShort_name());
+        assertEquals(expectedData.getDescription(), actualData.getDescription());
+        assertEquals(expectedData.getGroup_type_id(), actualData.getGroup_type_id());
+        assertEquals(expectedData.getOrganization_id(), actualData.getOrganization_id());
+
+
+    }
+}
--- src/test/java/utilities/Authentication.java
+++ src/test/java/utilities/Authentication.java
@@ -14,9 +14,13 @@ public class Authentication {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://qa-gm3.quaspareparts.com/");
         driver.findElement(By.linkText("Login")).click();
-        driver.findElement(By.id("username")).sendKeys("bo@testevolve.com");
-        driver.findElement(By.id("password")).sendKeys("FarahAl_huz@1234");
-        driver.findElement(By.id("button")).click();
+        driver.findElement(By.id("username")).sendKeys("dsdt2004@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("IQJe8N4QkMruFHc");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
         return driver.manage().getCookieNamed("GSESSIONID").getValue();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSessionId());
     }
 }
